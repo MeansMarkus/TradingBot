9cd90cdce88ad3af3359131080aa63a0eaf326cf
MeansMarkus <markus.adam2014@gmail.com>
2025-06-29 19:31:52 -0500

    Refined logic using cursor

--- config.py
+++ config.py
@@ -12,13 +12,13 @@ class Config:
     PASSPHRASE = os.getenv('PASSPHRASE', '')  # For some exchanges like OKX
     
     # Trading Configuration
-    SYMBOL = 'ES'  # E-mini S&P 500 futures
+    SYMBOL = 'BTC/USDT:USDT'  # Bitcoin perpetual futures on Binance
     TIMEFRAME = '1m'  # 1 minute candles
     PAPER_TRADING = True  # Start with paper trading
     
     # Risk Management
-    MAX_POSITION_SIZE = 1  # Number of contracts
-    MAX_DAILY_LOSS = 1000  # Maximum daily loss in USD
+    MAX_POSITION_SIZE = 0.01  # Number of contracts (0.01 BTC for paper trading)
+    MAX_DAILY_LOSS = 100  # Maximum daily loss in USD
     STOP_LOSS_PERCENT = 2.0  # Stop loss percentage
     TAKE_PROFIT_PERCENT = 4.0  # Take profit percentage
     
